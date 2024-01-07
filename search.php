11069fc230aac63ba8b0c41c209ccd7a1896b6f2
Fulop-Krisztian <50krissssz22@gmail.com>
2024-01-07 15:29:06 +0100

    API testing...

--- search.php
+++ search.php
@@ -1,27 +1,23 @@
 <?php
 // Connect database to search from
-require 'connectdb.php';
+require 'PHP/connectdb.php';
 
 
 
 if (isset($_GET['search'])) {
     $search = $_GET['search'];
-
-    // SQL query to search in 'items' table
+    echo $search;
     $sql = "SELECT * FROM test_table";
-
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // Output data of each row
         while ($row = $result->fetch_assoc()) {
             echo "Item: " . $row['item_name'] . "<br>";
-            // Display other item details as needed
         }
     } else {
         echo "No results found.";
     }
 }
 
-$conn->close(); // Close the database connection
+$conn->close();
 ?>
